35983757f89a1e5e8f7b838a76427141c96519b5
Muhammad Talha <mtalhaasifshazad@gmail.com>
2024-11-27 22:44:16 +0500

    Code Expption Handling

---
+++ src/exception.py
@@ -0,0 +1,16 @@
+import sys
+
+def error_message_info(error, error_detail:sys)->str:
+    _,_,exec_tb = error_detail.exc_info()
+    file_name = exec_tb.tb_frame.f_code.co_filename()
+    error_message = f"The error  occured in python script name {file_name}, line no {exec_tb.tb_lineno}, and error message {str(error)}"
+
+    return error_message
+
+class CustomException(Exception):
+    def __init__(self,error_message,error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_info(error_message,error_detail=error_detail)
+    
+    def __str__(self):
+        return self.error_message
